2953cc5b0e2294464f0e12cb3b315ecd59c55e40
Raman <rseviaryn@gmail.com>
2021-06-13 12:59:03 -0400

    Player Spawn

--- Core/MainGame.gd
+++ Core/MainGame.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+onready var mob = load("res://NPC/NPC.tscn")
+onready var powerup = load("res://PowerUps/PowerUp.tscn")
+
 func _ready():
+	randomize()
 	$WreckingBall.character = $Character
 	$Character.wreckingBall = $WreckingBall
 	
@@ -10,3 +14,21 @@ func _ready():
 
 func setUpLabyrinth():
 	$Character.updateHealth()
+	
+	#player spawn
+	var spawn = $PlayerSpawnPoints.get_child(randi()%$PlayerSpawnPoints.get_child_count())
+	$Character.position = spawn.position
+	$WreckingBall.position = spawn.position
+	
+	#monster spawn
+	for p in $MonsterSpawnPoints.get_children():
+		var new_mob = mob.instance()
+		new_mob.position = p.position
+		#new_mob.nav_2d = $GameMap/Navigation2D
+		#new_mob.character = $Character
+		add_child(new_mob)
+	
+	for p in $PowerUpSpawnPoints.get_children():
+		var new_powerup = powerup.instance()
+		new_powerup.position = p.position
+		add_child(new_powerup)
